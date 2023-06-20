e18de7dc46465d316c408945a03424ade4ee512a
Muthiani <bluntcoder254@gmail.com>
2023-06-20 01:52:51 -0700

    Corrections

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,20 +3,21 @@
 /**
  * main - Entry point
  *
- * Return: always (0) success
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
-	int i = 0;
-	
-	while (i <= 9)
+	int i;
+
+	i = 0;
+	while (i < 10)
 	{
 		putchar('0' + i);
 		if (i != 9)
 		{
 			putchar(',');
-			putchar(' ');
+			putchat(' ');
 		}
 		i++;
 	}
