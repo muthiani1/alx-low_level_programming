81bc0002a1f561ded712b2943189ac71fc41fe28
Muthiani <bluntcoder254@gmail.com>
2023-06-29 00:41:50 -0700

    0x03

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,16 +9,18 @@
  */
 
 int largest_number(int a, int b, int c)
-{int largest;
-	if (a > b && a > c)
+{
+	int largest;
+
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b  > c)
+	else if (b >= a && a >= c)
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
+	else
 	{
 		largest = c;
 	}
